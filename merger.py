9a8f4571856e3512b732ca7b581622237050d732
Balazs Juhasz <juhaszbal@gmail.com>
2020-12-16 21:32:18 +0100

    add pdf merger

--- merger.py
+++ merger.py
@@ -6,8 +6,14 @@ inputs = sys.argv[1:]
 
 
 def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+
     for pdf in pdf_list:
-        print(pdf)
+        print(f'Merging: {pdf}')
+        merger.append(pdf)
+
+    merger.write('superpdf.pdf')
 
 
-pdf_combiner(inputs)
+if __name__ == "__main__":
+    pdf_combiner(inputs)
